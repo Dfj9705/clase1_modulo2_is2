8f3477bd971ac32175ac6df347dcccf8cc923b1a
calo13 <carlologo96@gmail.com>
2023-07-04 11:36:32 -0600

    PRIMEROS CONCEPTOS

---
+++ script.js
@@ -0,0 +1,30 @@
+// TIPO NUMBER
+let numero = 15;
+// TIPO STRING
+let saludo = "hola mundo, desde js"
+// TIPO BOOLEAN
+let validacion = true;
+// TIPO NULL
+let data = null;
+// DEFINICION DE UN CONSTANTE
+const valorPi = 3.1416;
+// ALCANCE (SCOPE)
+// if(true){
+//     saludo = "hola mundo desde el if"
+//     numero = 10;
+//     // alert(numero)
+//     // alert(saludo)
+//     // valorPi = 5;
+// }
+
+
+// alert(typeof validacion)
+// alert(numero++)
+
+// function mostrarNumero(){
+//     let saludo = "hola desde la funcion"
+//     // alert(saludo)
+// }
+ 
+
+// mostrarNumero();
